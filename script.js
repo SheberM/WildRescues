371e308ee43423f6419aa6e8d818b788112d598c
SheberM <636555@csinow.edu>
2025-03-11 15:59:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+/* 
+Author: Sheber Mokua
+File Name: script.js
+Date: 02/20/2025
+*/
+
+// Hamburger menu function
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  document.getElementById("nav-links").classList.toggle("show");
+});
+
+// FAQ interactivity
+const questions = document.querySelectorAll("#questions p");
+const response = document.getElementById("response");
+
+questions.forEach((question, index) => {
+  question.addEventListener("click", function() {
+    let answers = [
+      "If you find an injured animal, contact us immediately at (555) 123-4567.",
+      "Yes! We welcome volunteers. Visit our 'Partnership' page to learn more.",
+      "We rescue raccoons, foxes, squirrels, birds, reptiles, and more.",
+      "Donations help us save more animals! Visit our 'Contact' page for details."
+    ];
+    response.textContent = answers[index];
+  });
+});
---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*  
+Author: Sheber Mokua  
+File Name: script.js  
+Date: 02/25/2025  
+*/
+
+// jQuery for hero image to consume the header window space  
+$(document).ready(function() {
+  $('.hero').height($(window).height());
+});
